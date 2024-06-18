043d820367df355de96e2b37831953fa1987bb5e
Digvijay Narayan <digvijayn1993@gmail.com>
2024-06-18 16:50:16 +0530

    mongodb connected using mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
 const express=require("express")
-
+const {mongoose } = require("mongoose")
+const cors=require("cors")
 const app = express()
+const dotenv=require("dotenv")
+
+
+const connectDB = async() => {
+    try {
+         await mongoose.connect(process.env.MONGO_URI)
+         console.log("mongodb connected")
+    } catch (err) {
+        console.log("err connecting to mongodb",err)
+    }
+}
+
+app.use(cors())
+dotenv.config()
+connectDB()
+app.use(express.json())
+
 
 
 app.listen(4000,()=>{
