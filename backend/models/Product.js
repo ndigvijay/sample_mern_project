bafde628b2bb63d4a7105ca124c36e004804e5a0
Digvijay Narayan <digvijayn1993@gmail.com>
2024-06-18 17:57:00 +0530

    added models

---
+++ backend/models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: { 
+        type: String
+    },
+    description: { 
+        type: String 
+    },
+    price: { 
+        type: Number
+    },
+    quantity: { 
+        type: Number
+    },
+    category: { 
+        type: String 
+    },
+});
+
+module.exports = mongoose.model("PRODUCTS", productSchema);
