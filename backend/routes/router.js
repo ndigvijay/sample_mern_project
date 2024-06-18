be11346022c72b1a0fb888e577d02639ce9daa4b
Digvijay Narayan <digvijayn1993@gmail.com>
2024-06-18 17:56:29 +0530

    completed routes

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,11 +1,13 @@
 const express=require("express")
+const {ProductAdd} =require("../controllers/product")
+const {ProductGet} =require("../controllers/product")
+const {ProductSearch} =require("../controllers/product")
 
 const app = express.Router()
 
-
-
-
-
+app.post('/products', ProductAdd);
+app.get('/products', ProductGet);
+app.get('/products/search', ProductSearch);
 
 
 module.exports=app
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const {mongoose } = require("mongoose")
 const cors=require("cors")
 const app = express()
 const dotenv=require("dotenv")
+const router=require("./routes/router")
 
 
 const connectDB = async() => {
@@ -18,6 +19,7 @@ app.use(cors())
 dotenv.config()
 connectDB()
 app.use(express.json())
+app.use("/api",router)
 
 
 
